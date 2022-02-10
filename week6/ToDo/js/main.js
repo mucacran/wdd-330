db73209c27ba49aa1a3d257b92168a694768e134
mucacran <el.mucacran.rasta@hotmail.com>
2022-02-10 17:01:03 -0500

    nuevos cambios desde la oficina

--- week6/ToDo/js/main.js
+++ week6/ToDo/js/main.js
@@ -1,38 +1,37 @@
 if(localStorage.getItem('nombreList') != null){
     var datosViejos = JSON.parse(localStorage.getItem('nombreList'));
-    datosViejos.forEach( item => (){ var le = '';});
+    datosViejos.forEach( item => creandoLi(item));
+}else{
+    var datosViejos = '';
 }
 
 
-function anadirItem(listItemTodo,datosViejos){
-    //definiendo llamadas
-    //let listItemTodo = document.querySelector('#itemListToDo');
-    let listActividades = document.querySelector('#listActividades');
-    const ul = document.querySelector('#linkList');
-
-
-    
-    //creando
+function creandoLi(listItemTodo){
+    const ul = document.getElementById('linkList');
     const li = document.createElement('li');
     const div = document.createElement('div');
-    //contenido = listItemTodo.value
-    div.innerHTML = listItemTodo;
+
+    const i = document.createElement('i');
+    i.setAttribute('class','eliminar');
+    i.setAttribute('onclick','eliminar(event)');
+    i.innerHTML = 'X';
+
+    const ii = document.createElement('i');
+    ii.setAttribute('class','done');
+    ii.setAttribute('onclick','hecho(event)');
+    ii.innerHTML = 'Done';
+
+    
+    div.innerHTML = '<div class="contenidoList"><p>' + listItemTodo + '</p></div>';
+    div.appendChild(i);
+    div.appendChild(ii);
+    
+    
 
     //añadiendo
     ul.appendChild(li);
     li.appendChild(div);
-
-    
 }
-
-/*
-toDoList = [toDo];
-const todo = {
-    "id" : timestamp,
-    "content": string,
-    "completed": bool
-}
-*/
 
 function almacenar(nombre,valor){
     localStorage.setItem(nombre,valor);
@@ -43,17 +42,43 @@ function almacenar(nombre,valor){
 function guardar(){
     let listItemTodo = document.querySelector('#itemListToDo').value;
 
-    if(localStorage.getItem('nombreList') == null){
-        almacenar('nombreList','[]');
+    if(listItemTodo != ''){
+        if(localStorage.getItem('nombreList') == null){
+            almacenar('nombreList','[]');
+        }
+    
+        datosViejos = JSON.parse(localStorage.getItem('nombreList'));
+        datosViejos.push(listItemTodo);
+    
+        almacenar('nombreList',JSON.stringify(datosViejos));
+    
+    
+        creandoLi(listItemTodo);
+    }
+    else{
+        alert('Error: llene el campo antes de hacer click');
     }
 
-    datosViejos = JSON.parse(localStorage.getItem('nombreList'));
-    datosViejos.push(listItemTodo);
+    
+}
 
-    almacenar('nombreList',JSON.stringify(datosViejos));
+function borrarTodo(){
+    localStorage.clear();
+    const ul = document.getElementById('linkList');
+    document.querySelector('#itemListToDo').value = '';
 
+    ul.innerText = '';//NO ERA LO MISMO QUE PONER innerHTML
+}
 
-    anadirItem(listItemTodo,datosViejos);
+function eliminar(event){
+    let div = event.path[1].querySelector('div > p');
+    console.log(div.innerText);
+
+    
+}
+
+function hecho(event){
+    console.log(event.path[1]);
 }
 
 //https://www.youtube.com/watch?v=2hJ1rTANVnk
