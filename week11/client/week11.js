ff1b741116dcecf332e27f71f39e21a50dd377c2
mucacran <el.mucacran.rasta@hotmail.com>
2022-03-19 18:25:26 -0500

    nuevos cambios

--- week11/client/week11.js
+++ week11/client/week11.js
@@ -17,16 +17,16 @@ loginForm.querySelector('button').addEventListener('click', () => {
 async function getPosts() {
     try {
       const data = await makeRequest('posts', 'GET', null, myAuth.token);
-    //   // make sure the element is shown
-    //   document.getElementById('content').classList.remove('hidden');
-    //   console.log(data);
-    //   var ul = document.getElementById('list');
-    //   ul.innerHTML = '';
-    //   for (var i = 0; i < data.length; i++) {
-    //     var li = document.createElement('li');
-    //     li.appendChild(document.createTextNode(data[i].title));
-    //     ul.appendChild(li);
-    //   }
+      // make sure the element is shown
+      document.getElementById('content').classList.remove('hidden');
+      console.log(data);
+      var ul = document.getElementById('list');
+      ul.innerHTML = '';
+      for (var i = 0; i < data.length; i++) {
+        var li = document.createElement('li');
+        li.appendChild(document.createTextNode(data[i].title));
+        ul.appendChild(li);
+      }
       myErrors.clearError();
     } catch (error) {
       // if there were any errors display them
@@ -34,25 +34,30 @@ async function getPosts() {
     }
   }
 
-  async function createPost() {
-    const form = document.forms.postForm;
-    console.dir(form);
-    if (form.title.validity.valid && form.content.validity.valid) {
-      myErrors.clearError();
-      const data = {
+  document.getElementById('createSubmit').addEventListener('click', () => {
+    createPost();
+  });
+
+async function createPost() {
+  const form = document.forms.postForm;
+    //console.dir(form);
+  console.log(form);
+  if (form.title.validity.valid && form.content.validity.valid) {
+        myErrors.clearError();
+        const data = {
         title: form.title.value,
         content: form.content.value
-      };
+        };
       try {
-        const res = await makeRequest('posts', 'POST', data, myAuth.token);
-        console.log('Post create:', data);
-        form.title.value = '';
-        form.content.value = '';
-        getPosts();
+          const res = await makeRequest('posts', 'POST', data, myAuth.token);
+          console.log('Post create:', data);
+          form.title.value = '';
+          form.content.value = '';
+          getPosts();
       } catch (error) {
-        myErrors.handleError(error);
+          myErrors.handleError(error);
       }
-    } else {
+     } else {
       myErrors.displayError({ message: 'Title and Content are required' });
-    }
-  }
+     }
+}
